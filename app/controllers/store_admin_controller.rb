95884c31eda8e54fc9beca1cef7a891e1e76a74b
Lynie McClimon <vmcclimon33@yahoo.com>
2020-03-05 02:06:41 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -8,7 +8,7 @@ class StoreAdminController < ApplicationController::Base
   def orders
       render :layout => "order_administration"
     end
-  
+
     def invoice
       render :layout => false
     end
